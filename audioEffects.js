667f8b42bdb001b0ba2f4db74278d4d1f9cf7ecc
UnnamedBruh <165609225+UnnamedBruh@users.noreply.github.com>
2025-06-11 09:02:08 -0500

    Update audioEffects.js

--- audioEffects.js
+++ audioEffects.js
@@ -1,1 +1,9 @@
-// real code is going to be added here
+const effectsList = [
+	[
+		"Gain",
+		"Multiplies each sample's value in the audio by a value, making the sound louder or quieter.",
+		'Multiplier: <input id="gain0" type="number" min="-10" max="10" step="0.001" value="2">',
+		1,
+		"gain"
+	]
+];
