b69299b2b6673e3977476cdca0666c607311ab11
UnnamedBruh <165609225+UnnamedBruh@users.noreply.github.com>
2025-06-11 14:54:46 -0500

    Update audioEffects.js

--- audioEffects.js
+++ audioEffects.js
@@ -1,3 +1,7 @@
+function identifier(x) {
+	return x;
+}
+
 const effectsList = [
 	[
 		"Gain",
@@ -10,9 +14,17 @@ const effectsList = [
 	[
 		"Playback Speed",
 		"Speeds up or slows down the audio.",
-		'Multiplier: <input id="speed0" type="number" min="0.5" max="10" step="0.001" value="2">',
-		1,
+		'Multiplier: <input id="speed0" type="number" min="0.5" max="10" step="0.001" value="2"><br>Smoothen Up Audio: <input id="speed1" type="checkbox" checked>',
+		2,
 		"speed",
-		[Number]
+		[Number, identifier]
+	],
+	[
+		"Resample Audio",
+		"Resamples the audio to a specific sample rate.",
+		'Samplerate: <select id="resample0"><option>11025</option><option>22050</option><option>44100</option><option>48000</option><option>96000</option></select><br>Smoothen Up Audio: <input id="resample1" type="checkbox" checked>',
+		2,
+		"resample",
+		[Number, identifier]
 	]
 ];
