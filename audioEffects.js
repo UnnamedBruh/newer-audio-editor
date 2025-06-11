5cd3606483db2d54234f9bec9371ceedffb4ca6f
UnnamedBruh <165609225+UnnamedBruh@users.noreply.github.com>
2025-06-11 09:54:43 -0500

    Update audioEffects.js

--- audioEffects.js
+++ audioEffects.js
@@ -4,13 +4,15 @@ const effectsList = [
 		"Amplifies each sample's value in the audio, making the sound louder or quieter.",
 		'Multiplier: <input id="gain0" type="number" min="-10" max="10" step="0.001" value="2">',
 		1,
-		"gain"
+		"gain",
+		[Number]
 	],
 	[
 		"Playback Speed",
 		"Speeds up or slows down the audio.",
-		'Multiplier: <input id="gain0" type="number" min="-10" max="10" step="0.001" value="2">',
+		'Multiplier: <input id="speed0" type="number" min="0.5" max="10" step="0.001" value="2">',
 		1,
-		"speed"
+		"speed",
+		[Number]
 	]
 ];
