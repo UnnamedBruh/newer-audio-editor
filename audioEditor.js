c1d42c4bf84dc7821f75e9834f10f8503637e183
UnnamedBruh <165609225+UnnamedBruh@users.noreply.github.com>
2025-06-11 15:47:15 -0500

    Update audioEditor.js

--- audioEditor.js
+++ audioEditor.js
@@ -39,6 +39,9 @@ effects["resample"] = function(buffer, targetSampleRate, shouldSmooth) {
 	if (targetSampleRate === buffer.sampleRate) return;
 
 	const speed = buffer.sampleRate / targetSampleRate;
+
+	skipSample = Math.round(480 * (targetSampleRate / 48000));
+	
 	_resampleAudio(buffer, speed, shouldSmooth);	
 	buffer.sampleRate = targetSampleRate;
 }
