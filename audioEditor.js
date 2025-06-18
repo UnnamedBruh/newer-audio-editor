cd0a405f8f6d1ac5a2fe28f92992c3f0e34abca9
UnnamedBruh <165609225+UnnamedBruh@users.noreply.github.com>
2025-06-18 18:25:24 -0500

    Update audioEditor.js

--- audioEditor.js
+++ audioEditor.js
@@ -6,11 +6,7 @@ function interpolate(x, y, z) {
 
 function _resampleAudio(buffer, speed, shouldSmooth) {
 	const bufferLength = buffer.audioData.length / speed;
-	const variable = buffer.audioData instanceof Uint8Array ? new Uint8Array(Math.round(bufferLength)) :
-		buffer.audioData instanceof Int16Array ? new Int16Array(Math.round(bufferLength)) :
-		buffer.audioData instanceof Uint32Array ? new Uint32Array(Math.round(bufferLength)) :
-		buffer.audioData instanceof Float32Array ? new Float32Array(Math.round(bufferLength)) :
-		new Float64Array(Math.round(bufferLength));
+	const variable = buffer.audioData instanceof Float32Array ? new Float32Array(Math.round(bufferLength)) : new Float64Array(Math.round(bufferLength));
 
 	let v = Math.round(bufferLength) - 1;
 
@@ -51,30 +47,32 @@ effects["speed"] = function(buffer, speed, shouldSmooth) {
 
 effects["gain"] = function(buffer, multiplier) {
 	if (multiplier === 1) return;
-	if (buffer.audioData instanceof Float32Array || buffer.audioData instanceof Float64Array || buffer.audioData instanceof Int16Array) {
-		if (multiplier === 0) buffer.audioData.fill(0); else if (multiplier === -1) {
-			const len = buffer.audioData.length, data = buffer.audioData;
-			for (let i = 0; i < len; i++) {
-				data[i] = -data[i];
-			}
-		} else {
-			const len = buffer.audioData.length, data = buffer.audioData;
-			for (let i = 0; i < len; i++) {
-				data[i] *= multiplier;
-			}
+	if (multiplier === 0) buffer.audioData.fill(0); else if (multiplier === -1) {
+		const len = buffer.audioData.length, data = buffer.audioData;
+		for (let i = 0; i < len; i++) {
+			data[i] = -data[i];
 		}
 	} else {
-		const dec = buffer.audioData instanceof Uint8Array ? 128 : 2147483648;
-		if (multiplier === 0) buffer.audioData.fill(dec); else if (multiplier === -1) {
-			const len = buffer.audioData.length, data = buffer.audioData;
-			for (let i = 0; i < len; i++) {
-				data[i] = -(data[i] - dec) + dec;
-			}
-		} else {
-			const len = buffer.audioData.length, data = buffer.audioData;
-			for (let i = 0; i < len; i++) {
-				data[i] = (data[i] - dec) * multiplier + dec;
-			}
+		const len = buffer.audioData.length, data = buffer.audioData;
+		for (let i = 0; i < len; i++) {
+			data[i] *= multiplier;
+		}
+	}
+}
+
+effects["quantize"] = function(buffer, bits) {
+	bits = bits - 1;
+	if (bits >= 32) return;
+	const precision = Math.pow(2, -bits), n = Math.pow(2, bits);
+	const len = buffer.audioData.length, pointer = buffer.audioData;
+
+	if (bits === 1) {
+		for (let i = 0; i < len; i++) {
+			pointer[i] = Math.trunc(pointer[i]);
+		}
+	} else {
+		for (let i = 0; i < len; i++) {
+			pointer[i] = Math.trunc(pointer[i] * n) * precision;
 		}
 	}
 }
