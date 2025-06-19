59547bc4f9570bc64e404ac95ff9e53ba0968093
UnnamedBruh <165609225+UnnamedBruh@users.noreply.github.com>
2025-06-18 23:44:12 -0500

    Update audioEffects.js

--- audioEffects.js
+++ audioEffects.js
@@ -38,7 +38,7 @@ const effectsList = [
 	[
 		"Smooth Audio",
 		"Interpolates samples into each other linearly or with an accumulator, creating a \"lowpass\" effect.",
-		'<a id="custom">Smoothing Threshold (measured in samples)</a>: <input id="smooth0" type="number" min="1" max="256" step="1" value="4"><br>Blending Method: <select id="smooth1" oninput="document.getElementById(\'custom\').textContent = this.value === \'a\' ? \'Blend\' : \'\';"><option value="n">no-interpolating</option><option value="l">linear interpolation</option><option value="a">interpolate accumulator</option></select><br>',
+		'<a id="custom">Smoothing Threshold (measured in samples)</a>: <input id="smooth0" type="number" min="1" max="256" step="1" value="4"><br>Blending Method: <select id="smooth1" oninput="document.getElementById(\'custom\').textContent = this.value === \'a\' ? \'Interpolation Level (as percentage)\' : \'Smoothing Threshold\';"><option value="n">no-interpolating</option><option value="l">linear interpolation</option><option value="a">accumulator-based interpolation</option></select>',
 		2,
 		"smooth",
 		[Number, identifier]
