143bd30fc20f0c2e8503058ddb5e06b5a0b73c9f
UnnamedBruh <165609225+UnnamedBruh@users.noreply.github.com>
2025-09-19 14:05:51 -0600

    Update audioEditor.js

--- audioEditor.js
+++ audioEditor.js
@@ -1,4 +1,6 @@
-function muLawCompress(a,t=255){return Math.sign(a)*Math.log1p(t*Math.abs(a))/Math.log1p(t)}function muLawExpand(a,t=255){return Math.sign(a)*(1/t)*(Math.pow(1+t,Math.abs(a))-1)}function muLawQuantize(a,t=256,n=255){const u=muLawCompress(a,n);return muLawExpand(Math.floor((u+1)/2*(t-1))/(t-1)*2-1,n)}
+const abs = Math.abs, sqrt = Math.sqrt, sign = Math.sign, cbrt = Math.cbrt, floor = Math.floor, log1p = Math.log1p, pow = Math.pow, round = Math.round;
+
+function muLawCompress(a,t=255){return sign(a)*log1p(t*abs(a))/log1p(t)}function muLawExpand(a,t=255){return sign(a)*(1/t)*(pow(1+t,abs(a))-1)}function muLawQuantize(a,t=256,n=255){const u=muLawCompress(a,n);return muLawExpand(floor((u+1)/2*(t-1))/(t-1)*2-1,n)}
 
 const effects = Object.create(null);
 
@@ -8,25 +10,26 @@ function interpolate(x, y, z) {
 
 function _resampleAudio(buffer, speed, shouldSmooth) {
 	const bufferLength = buffer.audioData.length / speed;
-	const variable = buffer.audioData instanceof Float32Array ? new Float32Array(Math.round(bufferLength)) : new Float64Array(Math.round(bufferLength));
+	const variable = buffer.audioData instanceof Float32Array ? new Float32Array(round(bufferLength)) : new Float64Array(round(bufferLength));
 
 	let v = Math.round(bufferLength) - 1;
+	let integerfunction = Number.isInteger;
 
 	if (shouldSmooth && !Number.isInteger(speed)) {
 		let x = 0, y = 0, z = buffer.audioData;
 		for (let i = 0; i < v; i++) {
 			x = i * speed;
-			if (Number.isInteger(x)) {
+			if (integerfunction(x)) {
 				variable[i] = z[x];
 			} else {
-				y = Math.floor(x);
+				y = floor(x);
 				variable[i] = interpolate(z[y], z[y + 1], x - y);
 			}
 		}
 	} else {
 		let x = 0, y = 0, z = buffer.audioData;
 		for (let i = 0; i < v; i++) {
-			variable[i] = z[Math.floor(i * speed)];
+			variable[i] = z[floor(i * speed)];
 		}
 	}
 
@@ -62,17 +65,19 @@ effects["gain"] = function(buffer, multiplier) {
 	}
 }
 
+const f16round = Math.f16round;
+
 effects["quantize"] = function(buffer, bits, which) {
 	if (bits >= 32) return;
 	bits = bits - 1;
 	const precision = Math.pow(2, -bits), n = Math.pow(2, bits);
 	const len = buffer.audioData.length, pointer = buffer.audioData;
-	const trunc = which === "r" ? Math.round : (which === "t" ? Math.trunc : (which === "f" ? Math.floor : Math.ceil));
+	const trunc = which === "r" ? round : (which === "t" ? trunc : (which === "f" ? floor : ceil));
 
 	if (which === "fl") {
-		if ("f16round" in Math) {
+		if (f16round) {
 			for (let i = 0; i < len; i++) {
-				pointer[i] = Math.f16round(pointer[i]);
+				pointer[i] = f16round(pointer[i]);
 			}
 		} else {
 			alert("Unfortunately, half-precision floats are not supported in your browser. You can switch your browser to the latest version of Google Chrome, Microsoft Edge, Safari or Mozilla Firefox.");
@@ -105,22 +110,22 @@ effects["smooth"] = function(buffer, samples, method) {
 		let z = buffer.audioData, perc = samples / 100, f, p;
 		for (let i = 1; i < v; i++) {
 			f = z[i]; p = z[i - 1];
-			z[i] = interpolate(f, p, perc * Math.pow(1 - Math.abs(f - p), 2));
+			z[i] = interpolate(f, p, perc * pow(1 - abs(f - p), 2));
 		}
 	} else {
-		const variable = buffer.audioData instanceof Float32Array ? new Float32Array(Math.round(v)) : new Float64Array(Math.round(v));
+		const variable = buffer.audioData instanceof Float32Array ? new Float32Array(round(v)) : new Float64Array(round(v));
 
 		if (method === "l") {
 			let y = 0, z = buffer.audioData;
 			let samplesFrac = 1 / samples;
 			for (let i = 0; i < v; i++) {
-				y = Math.floor(i / samples) * samples;
+				y = floor(i / samples) * samples;
 				variable[i] = interpolate(z[y], z[y + samples] || 0, (i * samplesFrac) % 1);
 			}
 		} else if (method === "n") {
 			let x = 0, y = 0, z = buffer.audioData;
 			for (let i = 0; i < v; i++) {
-				variable[i] = z[Math.floor(i / samples) * samples];
+				variable[i] = z[floor(i / samples) * samples];
 			}
 		} else {
 			let acc = 0;
@@ -142,23 +147,23 @@ effects["distort"] = function(buffer, perc, method) {
 	if (perc === 0) return; else if (perc === 100) {
 		if (method === "s") {
 			for (let i = 0; i < v; i++) {
-				z[i] = Math.sqrt(Math.abs(z[i])) * Math.sign(z[i]);
+				z[i] = sqrt(abs(z[i])) * sign(z[i]);
 			}
 		} else if (method === "c") {
 			for (let i = 0; i < v; i++) {
-				z[i] = Math.cbrt(Math.abs(z[i])) * Math.sign(z[i]);
+				z[i] = cbrt(abs(z[i])) * sign(z[i]);
 			}
 		} else if (method === "sq") {
 			let x = 0;
 			for (let i = 0; i < v; i++) {
-				x = Math.abs(z[i]);
-				z[i] = x * x * Math.sign(z[i]);
+				x = abs(z[i]);
+				z[i] = x * x * sign(z[i]);
 			}
 		} else {
 			let x = 0;
 			for (let i = 0; i < v; i++) {
-				x = Math.abs(z[i]);
-				z[i] = x / (Math.floor(x * 8) / 8) * Math.sign(z[i]);
+				x = abs(z[i]);
+				z[i] = x / (floor(x * 8) / 8) * sign(z[i]);
 				if (z[i] === Infinity) z[i] = 1; else if (z[i] === -Infinity) z[i] = -1;
 			}
 		}
@@ -166,25 +171,59 @@ effects["distort"] = function(buffer, perc, method) {
 		perc = perc / 100;
 		if (method === "s") {
 			for (let i = 0; i < v; i++) {
-				z[i] = interpolate(z[i], Math.sqrt(Math.abs(z[i])) * Math.sign(z[i]), perc);
+				z[i] = interpolate(z[i], sqrt(abs(z[i])) * sign(z[i]), perc);
 			}
 		} else if (method === "c") {
 			for (let i = 0; i < v; i++) {
-				z[i] = interpolate(z[i], Math.cbrt(Math.abs(z[i])) * Math.sign(z[i]), perc);
+				z[i] = interpolate(z[i], cbrt(abs(z[i])) * sign(z[i]), perc);
 			}
 		} else if (method === "sq") {
 			let x = 0;
 			for (let i = 0; i < v; i++) {
-				x = Math.abs(z[i]);
-				z[i] = interpolate(x, x * x * Math.sign(z[i]), perc);
+				x = abs(z[i]);
+				z[i] = interpolate(x, x * x * sign(z[i]), perc);
 			}
 		} else {
 			let x = 0;
 			for (let i = 0; i < v; i++) {
-				x = Math.abs(z[i]);
-				z[i] = interpolate(x, x / (Math.floor(x * 8) / 8) * Math.sign(z[i]), perc);
+				x = abs(z[i]);
+				z[i] = interpolate(x, x / (floor(x * 8) / 8) * sign(z[i]), perc);
 				if (z[i] === Infinity) z[i] = 1; else if (z[i] === -Infinity) z[i] = -1;
 			}
 		}
+	}
+}
+
+effects["echo"] = function(buffer, volume, echoes, delay, volumeGainer) {
+	if (echoes < 1 || volume === 0 || delay < 0 || volumeGainer <= 0) return;
+	if (delay === 0) {
+		echo["gain"](buffer, 1 + Math.pow(volume * echoes, -volumeGainer));
+		return;
+	}
+	volume = volume * 0.01;
+	delay = round(delay * buffer.sampleRate);
+
+	const v = buffer.audioData.length;
+	let newData;
+	if (!Object.prototype.hasOwnProperty.call(buffer.audioData, "slice")) {
+		newData = buffer.audioData instanceof Float32Array ? new Float32Array(round(v)) : new Float64Array(round(v));
+		newData.set(buffer.audioData);
+	} else {
+		newData = buffer.audioData.slice();
+	}
+
+	const audioPointer = buffer.audioData;
+
+	let delayAudio = delay;
+	let vol = volume;
+
+	for (let i = 0; i < echoes; i++) {
+		let k = 0;
+		if (delayAudio > v || vol === 0) break;
+		for (let j = delayAudio; j < v; j++, k++) {
+			audioPointer[j] += newData[k] * vol;
+		}
+		delayAudio += delay;
+		vol = vol * volumeGainer;
 	}
 }
