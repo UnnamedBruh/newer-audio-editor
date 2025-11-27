92f2806f23e46d040d2aac062a4c6e8459bd65aa
UnnamedBruh <165609225+UnnamedBruh@users.noreply.github.com>
2025-11-27 12:29:12 -0500

    Update audioEditor.js

--- audioEditor.js
+++ audioEditor.js
@@ -341,6 +341,7 @@ effects["noise"] = function(buffer, noiseType, volume, isAlgorithmistic) {
 	}
 	if (isAlgorithmistic) {
 		if (noiseType === "bn") { // GPT-5.0 Mini wrote this implementation, but I adapted it to this function's standards.
+			volume *= 0.5;
 			// Algorithmistic brown: cumulative sum of small white noise steps
 			let last = 0;
 			const step = 0.2;
@@ -355,7 +356,7 @@ effects["noise"] = function(buffer, noiseType, volume, isAlgorithmistic) {
 			let b0=0, b1=0, b2=0, b3=0, b4=0, b5=0, b6=0, pink=0, white=0;
 			if (volume === 1) {
 				for (let i = 0; i < len; i++) {
-					white = rand(); 
+					white = 2 * rand() - 1; 
 					b0 = 0.99886 * b0 + white * 0.0555179; // Start mark
 					b1 = 0.99332 * b1 + white * 0.0750759;
 					b2 = 0.969 * b2 + white * 0.153852;
@@ -364,20 +365,20 @@ effects["noise"] = function(buffer, noiseType, volume, isAlgorithmistic) {
 					b5 = -0.7616 * b5 - white * 0.016898;
 					pink = b0 + b1 + b2 + b3 + b4 + b5 + b6 + white * 0.5362;
 					b6 = white * 0.115926; // End mark
-					data[i] += pink;
+					data[i] += pink * 0.11;
 				}
 			} else {
 				for (let i = 0; i < len; i++) {
-					white = rand();
+					white = 2 * rand() - 1;
 					b0 = 0.99886 * b0 + white * 0.0555179;
-					b1 = 0.99332 * b1 + white * 0.0750759;7
+					b1 = 0.99332 * b1 + white * 0.0750759;
 					b2 = 0.969 * b2 + white * 0.153852;
 					b3 = 0.8665 * b3 + white * 0.3104856;
 					b4 = 0.55 * b4 + white * 0.5329522;
 					b5 = -0.7616 * b5 - white * 0.016898;
 					pink = b0 + b1 + b2 + b3 + b4 + b5 + b6 + white * 0.5362;
 					b6 = white * 0.115926;
-					data[i] += pink * volume;
+					data[i] += pink * volume * 0.11;
 				}
 			}
 		}
