11cf4b3a36c3c823cb996fdec3629bb22a31183b
UnnamedBruh <165609225+UnnamedBruh@users.noreply.github.com>
2025-12-10 00:45:10 -0500

    Update audioExporter.js

--- audioExporter.js
+++ audioExporter.js
@@ -679,7 +679,7 @@ AudioExporter.prototype.convertToSol = async function(buffer2) { // https://wiki
 		new Uint8Array(view.buffer).set(buff.subarray(44, buff.length), offset);
 	} else {
 		this.encoding = temp;
-		const SOLTable3bit = new Uint8Array([0,1,2,3,6,0xA,0xF,0x15]);const SOLTable7bit = new Uint16Array([0x0,	0x8,	 0x10,	0x20,	0x30,	0x40,	0x50,	0x60,	0x70,	0x80,	0x90,	0xA0,	0xB0,	0xC0,	0xD0,	0xE0,	0xF0,	0x100,	0x110,	0x120,	0x130,	0x140,	0x150,	0x160,	0x170,	0x180,	0x190,	0x1A0,	0x1B0,	0x1C0,	0x1D0,	0x1E0,	0x1F0,	0x200,	0x208,	0x210,	0x218,	0x220,	0x228,	0x230,	0x238,	0x240,	0x248,	0x250,	0x258,	0x260,	0x268,	0x270,	0x278,	0x280,	0x288,	0x290,	0x298,	0x2A0,	0x2A8,	0x2B0,	0x2B8,	0x2C0,	0x2C8,	0x2D0,	0x2D8,	0x2E0,	0x2E8,	0x2F0,	0x2F8,	0x300,	0x308,	0x310,	0x318,	0x320,	0x328,	0x330,	0x338,	0x340,	0x348,	0x350,	0x358,	0x360,	0x368,	0x370,	0x378,	0x380,	0x388,	0x390,	0x398,	0x3A0,	0x3A8,	0x3B0,	0x3B8,	0x3C0,	0x3C8,	0x3D0,	0x3D8,	0x3E0,	0x3E8,	0x3F0,	0x3F8,	0x400,	0x440,	0x480,	0x4C0,	0x500,	0x540,	0x580,	0x5C0,	0x600,	0x640,	0x680,	0x6C0,	0x700,	0x740,	0x780,	0x7C0,	0x800,	0x900,	0xA00,	0xB00,	0xC00,	0xD00,	0xE00,	0xF00,	0x1000, 0x1400, 0x1800, 0x1C00, 0x2000, 0x3000, 0x4000]);
+		let SOLTable3bit = new Uint8Array([0,1,2,3,6,0xA,0xF,0x15]);const SOLTable7bit = new Uint16Array([0x0,	0x8,	 0x10,	0x20,	0x30,	0x40,	0x50,	0x60,	0x70,	0x80,	0x90,	0xA0,	0xB0,	0xC0,	0xD0,	0xE0,	0xF0,	0x100,	0x110,	0x120,	0x130,	0x140,	0x150,	0x160,	0x170,	0x180,	0x190,	0x1A0,	0x1B0,	0x1C0,	0x1D0,	0x1E0,	0x1F0,	0x200,	0x208,	0x210,	0x218,	0x220,	0x228,	0x230,	0x238,	0x240,	0x248,	0x250,	0x258,	0x260,	0x268,	0x270,	0x278,	0x280,	0x288,	0x290,	0x298,	0x2A0,	0x2A8,	0x2B0,	0x2B8,	0x2C0,	0x2C8,	0x2D0,	0x2D8,	0x2E0,	0x2E8,	0x2F0,	0x2F8,	0x300,	0x308,	0x310,	0x318,	0x320,	0x328,	0x330,	0x338,	0x340,	0x348,	0x350,	0x358,	0x360,	0x368,	0x370,	0x378,	0x380,	0x388,	0x390,	0x398,	0x3A0,	0x3A8,	0x3B0,	0x3B8,	0x3C0,	0x3C8,	0x3D0,	0x3D8,	0x3E0,	0x3E8,	0x3F0,	0x3F8,	0x400,	0x440,	0x480,	0x4C0,	0x500,	0x540,	0x580,	0x5C0,	0x600,	0x640,	0x680,	0x6C0,	0x700,	0x740,	0x780,	0x7C0,	0x800,	0x900,	0xA00,	0xB00,	0xC00,	0xD00,	0xE00,	0xF00,	0x1000, 0x1400, 0x1800, 0x1C00, 0x2000, 0x3000, 0x4000]);
 		let CurSample = 0x80;
 		let Difference = 0x00;
 		function chooseIndexClosest(array, value) {
@@ -691,30 +691,65 @@ AudioExporter.prototype.convertToSol = async function(buffer2) { // https://wiki
 			return array[array.length - 1];
 		}
 		if (bits === 4) {
-			if (numOfChannels === 1) {
-				for (let i = 0; i < len; i += 2) {
-					let currentSample = 0;
-					CurSample = (samples[i] + 1) * 0x7F;
-					Difference = CurSample - Difference;
-					currentSample = (chooseIndexClosest(SOLTable3bit, Math.abs(Difference)) | (Difference<0?8:0));
-					if (i + 1 > len) break;
-					CurSample = (samples[i+1] + 1) * 0x7F;
-					Difference = CurSample - Difference;
-					currentSample |= (chooseIndexClosest(SOLTable3bit, Math.abs(Difference)) | (Difference<0?8:0)) << 4;
-					view.setUint8(offset++, currentSample);
+			if (this.encoding === "dpcmold") {
+				if (numOfChannels === 1) {
+					for (let i = 0; i < len; i += 2) {
+						let currentSample = 0;
+						CurSample = (samples[i] + 1) * 0x7F;
+						Difference = CurSample - Difference;
+						currentSample = chooseIndexClosest(SOLTable3bit, Math.abs(Difference));
+						if (Difference < 0) currentSample = (~currentSample) & 0xF;
+						if (i + 1 > len) break;
+						CurSample = (samples[i+1] + 1) * 0x7F;
+						Difference = CurSample - Difference;
+						let or = (chooseIndexClosest(SOLTable3bit, Math.abs(Difference))) << 4;
+						if (Difference < 0) or = (~or) & 0xF0;
+						currentSample |= or;
+						view.setUint8(offset++, currentSample);
+					}
+				} else {
+					let Difference2 = 0x00;
+					for (let i = 0; i < len; i++) {
+						let currentSample = 0;
+						CurSample = (samples[i] + 1) * 0x7F;
+						Difference = CurSample - Difference;
+						currentSample = chooseIndexClosest(SOLTable3bit, Math.abs(Difference)) << 4;
+						if (Difference < 0) currentSample = (~currentSample) & 0xF0;
+						if (i + 1 > len) break;
+						CurSample = (buffer2[i] + 1) * 0x7F;
+						Difference2 = CurSample - Difference2;
+						let or = chooseIndexClosest(SOLTable3bit, Math.abs(Difference2));
+						if (Difference2 < 0) or = (~or) & 0xF;
+						currentSample |= or;
+						view.setUint8(offset++, currentSample);
+					}
 				}
 			} else {
-				let Difference2 = 0x00;
-				for (let i = 0; i < len; i++) {
-					let currentSample = 0;
-					CurSample = (samples[i] + 1) * 0x7F;
-					Difference = CurSample - Difference;
-					currentSample = (chooseIndexClosest(SOLTable3bit, Math.abs(Difference)) | (Difference<0?8:0)) << 4;
-					if (i + 1 > len) break;
-					CurSample = (buffer2[i] + 1) * 0x7F;
-					Difference2 = CurSample - Difference;
-					currentSample |= chooseIndexClosest(SOLTable3bit, Math.abs(Difference2)) | (Difference2<0?8:0);
-					view.setUint8(offset++, currentSample);
+				if (numOfChannels === 1) {
+					for (let i = 0; i < len; i += 2) {
+						let currentSample = 0;
+						CurSample = (samples[i] + 1) * 0x7F;
+						Difference = CurSample - Difference;
+						currentSample = (chooseIndexClosest(SOLTable3bit, Math.abs(Difference)) | (Difference<0?8:0));
+						if (i + 1 > len) break;
+						CurSample = (samples[i+1] + 1) * 0x7F;
+						Difference = CurSample - Difference;
+						currentSample |= (chooseIndexClosest(SOLTable3bit, Math.abs(Difference)) | (Difference<0?8:0)) << 4;
+						view.setUint8(offset++, currentSample);
+					}
+				} else {
+					let Difference2 = 0x00;
+					for (let i = 0; i < len; i++) {
+						let currentSample = 0;
+						CurSample = (samples[i] + 1) * 0x7F;
+						Difference = CurSample - Difference;
+						currentSample = (chooseIndexClosest(SOLTable3bit, Math.abs(Difference)) | (Difference<0?8:0)) << 4;
+						if (i + 1 > len) break;
+						CurSample = (buffer2[i] + 1) * 0x7F;
+						Difference2 = CurSample - Difference2;
+						currentSample |= chooseIndexClosest(SOLTable3bit, Math.abs(Difference2)) | (Difference2<0?8:0);
+						view.setUint8(offset++, currentSample);
+					}
 				}
 			}
 		} else if (bits === 8) {
