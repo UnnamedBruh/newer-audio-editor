c8220494014b515fb636329c67d188ec340cd3de
UnnamedBruh <165609225+UnnamedBruh@users.noreply.github.com>
2025-12-20 11:47:37 -0500

    Update audioEditor.js

--- audioEditor.js
+++ audioEditor.js
@@ -677,7 +677,7 @@ effects["tvnormalize"] = function(exporter) {
 
 	trackVolume[trackVolume.length - 1] *= (len % chunkSize) / chunkSize;
 
-	function c(p) {p<-1?-1:p>1?1:p}
+	function yy(p) {p<-1?-1:p>1?1:p}
 
 	for (; i < len; i++) {
 		const x = i * chunkSizeDiv;
@@ -690,7 +690,7 @@ effects["tvnormalize"] = function(exporter) {
 			if (!isFinite(inter) || isNaN(inter) || inter === 0) inter = 2;
 			pointer[i] /= inter;
 		}
-		pointer[i] = c(pointer[i]);
+		pointer[i] = yy(pointer[i]);
 	}
 }
 
