c9a20af8935059b42d21267858d180eee627ab5e
UnnamedBruh <165609225+UnnamedBruh@users.noreply.github.com>
2026-04-26 22:28:58 -0400

    Update audioEffects.js

--- audioEffects.js
+++ audioEffects.js
@@ -314,12 +314,21 @@ const effectsList = [
 		[ispowof2, Number]
 	],
 	[
-		"FFT: Pitch Shift (Phase Vocoder)",
-		"Analyzes the audio data in chunks using a Fast Fourier Transform algorithm (indutny FFT.js), then pitch shifts the audio by accumulating the phase, and linearly distributing the magnitude. This effect has a noticeably less robotic quality, but may have some metallic \"chorus\"-like artifacts. Those artifacts can be reduced using the \"polished\" setting",
+		"FFT: Pitch Shift (Phase Vocoder Mono)",
+		"Analyzes the audio data in chunks using a Fast Fourier Transform algorithm (indutny FFT.js), then pitch shifts the audio by accumulating the phase, and linearly distributing the magnitude. This effect has a noticeably less robotic quality, but may have some metallic \"chorus\"-like artifacts. Those artifacts can be reduced using the \"polished\" setting.<br><a style=\"color: red\">THE \"POLISHED\" SETTING WILL WORK BEST ON MONO AUDIO. FOR STEREO AUDIO, THIS IS BEST ACHIEVED WITH THE STEREO VARIANT.</a>",
 		'Polished: <input id="fftpitchshiftbetter0" type="checkbox"><br>Pitch Multiplier (in octaves): <input id="fftpitchshiftbetter1" type="number" min="0.01" step="0.04" value="1.2" style="width: 100px">',
 		2,
 		"fftpitchshiftbetter",
 		[identifier, Number]
+	],
+	[
+		"FFT: Pitch Shift (Phase Vocoder Stereo)",
+		"Analyzes the audio data in chunks using a Fast Fourier Transform algorithm (indutny FFT.js), then pitch shifts the audio by accumulating the phase, and linearly distributing the magnitude. This effect has a noticeably less robotic quality, but may have some metallic \"chorus\"-like artifacts. Those artifacts can be reduced using the \"polished\" setting.",
+		'Polished: <input id="fftpitchshiftbetter0" type="checkbox"><br>Pitch Multiplier (in octaves): <input id="fftpitchshiftbetter1" type="number" min="0.01" step="0.04" value="1.2" style="width: 100px">',
+		2,
+		"fftpitchshiftbetterstereo",
+		[identifier, Number],
+		2
 	],
 	[
 		"FFT: As Raw Data",
